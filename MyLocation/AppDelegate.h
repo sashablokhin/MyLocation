38060e512490b03e3e24e29d48415e45ad736473
sashablokhin <sashablokhindev@gmail.com>
2016-05-25 13:10:53 +0300

    Create project

---
+++ MyLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyLocation
+//
+//  Created by Alexander Blokhin on 25.05.16.
+//  Copyright © 2016 Alexander Blokhin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
